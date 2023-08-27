ab4b413e0378bd03a5931c39396959581d1ab71d
Samuel-Ngigi <samuelngigi812@gmail.com>
2023-08-27 11:59:53 +0300

    innitial commit

---
+++ apikeys.js
@@ -0,0 +1,6 @@
+//js modules
+export  const dbConnection = mongoose.connect("mongodb://127.0.0.1:27017/NextTUsersDB");
+// export function dbString(){
+//     const db = mongoose.connect("mongodb://127.0.0.1:27017/NextTUsersDB");
+// }
+
